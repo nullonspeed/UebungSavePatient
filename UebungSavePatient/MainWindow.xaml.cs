24a66fc58541b9caa936183054d25938191bb2b4
nullonspeed <sebastian.langschneider@gmail.com>
2023-10-03 13:05:19 +0200

    Add project files.

---
+++ UebungSavePatient/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UebungSavePatient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    
+    public partial class MainWindow : Window
+    {
+        TextBox FirstName;
+        TextBox LastName;
+        DatePicker Birthday;
+        RadioButton Male;
+        RadioButton Female;
+        CheckBox cb;
+
+        //List<Patient> Patients;
+
+        ListBox listbox;
+        public MainWindow()
+        {
+            InitializeComponent();
+            FirstName = firstnametb;
+            LastName = lastnametb;
+            Birthday = birthdaydtp;
+            Male = maleradiobox;
+            Female = femaleradiobox;
+            cb = bedwettercheckbox;
+
+            //Patients = new List<Patient>();
+            listbox = listboxpatient;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(listbox.SelectedItem != null)
+            {
+               // Patients.Remove(Patients.Find(x=> x.ToString()==listbox.SelectedItem.ToString()));
+                listbox.Items.Remove(listbox.SelectedItem);
+            }
+
+
+        }
+
+      
+
+        private void Button_Click_PushToList(object sender, RoutedEventArgs e)
+        {
+            DateTime? dateTime = birthdaydtp.SelectedDate;
+            if (FirstName == null || LastName==null || Male.IsChecked ==false &&Female.IsChecked==false || dateTime ==null){
+                MessageBox.Show("Please fill out the values");
+            }
+            else
+            {
+               
+                DateOnly date = new DateOnly(dateTime.Value.Year, dateTime.Value.Month, dateTime.Value.Day);
+               Patient patient = new Patient();
+                patient.Firstname = FirstName.Text;
+                patient.Lastname = LastName.Text;
+                patient.Birthday = date;
+                if(Male.IsChecked==true){
+                patient.isMale = true;
+                }
+                else
+                {
+                    patient.isMale = false;
+                }
+                patient.isBedwetter=cb.IsChecked ==true;
+                listbox.Items.Add(patient.ToString());
+               // Patients.Add(patient);
+                
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            string line = "";
+            foreach(var item in listbox.Items)
+            {
+                line += item.ToString()+"\n";
+
+            }
+            SaveFileDialog sFD = new SaveFileDialog();
+            sFD.Filter = "CSV-Datei (*.csv)|*.csv";
+            if (sFD.ShowDialog() == true)
+            {
+                string filePath = sFD.FileName;
+
+                File.WriteAllText(filePath, line);
+
+            }
+
+           // File.WriteAllText()
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV Files (*.csv)|*.csv";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                try
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+                    foreach (string line in lines)
+                    {
+                        if (Patient.TryParse(line, out Patient p))
+                        {
+                           // Patients.Add(p);
+                            listbox.Items.Add(p.ToString());
+                        }
+                    }
+                    
+                    Console.WriteLine(lines);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred while reading the CSV file: {ex.Message}", "CSV File Reading Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            }
+        }
+}
---
+++ UebungSavePatient/Patient.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace UebungSavePatient
+{
+    class Patient
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateOnly Birthday { get; set; }
+        public bool isMale { get; set; }
+        public bool isBedwetter { get; set; }
+
+        public override string? ToString()
+        {
+            string gender = "";
+            if (isMale)
+            {
+                gender = "Male";
+            }
+            else
+            {
+                gender = "Female";
+            }
+            string bedwettertext = "";
+            if (isBedwetter)
+            {
+                bedwettertext = "bedwetter";
+            }
+            else
+            {
+                bedwettertext = "No bedwetter";
+            }
+
+            return $"{Firstname} {Lastname} {Birthday} [{gender}] - {bedwettertext}";
+        }
+
+        public static bool TryParse(string s, out Patient p)
+        {
+            string[] parts =s.Split(' ');
+            p = new Patient();
+
+            if (parts.Length >=5 )
+            {
+                p.Firstname = parts[0];
+                p.Lastname = parts[1];
+                string[]dateParts = parts[2].Split(".");
+                if (dateParts.Length == 3)
+                {
+                    int day = int.Parse(dateParts[0]);
+                    int month = int.Parse(dateParts[1]);
+                    int year = int.Parse(dateParts[2]);
+                    
+                    p.Birthday = new DateOnly(year, month, day);
+
+                    string gender = parts[3].Trim('[', ']');
+                    if (gender == "Male")
+                    {
+                        p.isMale = true;
+                    }
+                    else
+                    {
+                        p.isMale= false;
+                    }
+                    
+                    string bedwetterText = parts[5];
+                    if(bedwetterText == "bedwetter")
+                    {
+                        p.isBedwetter = true;
+                    }
+                    else
+                    {
+                        p.isBedwetter = false;
+                    }
+                    return true;
+
+                  }
+                else
+                {
+                    return false;
+                }
+
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
